c8bcbfd69bca647586e37c6ddc58d57011b78dfa
Nabira Rashid <163450706+nabirarashid@users.noreply.github.com>
2025-03-15 00:18:05 -0400

    Update simple_vector.py

--- simple_vector.py
+++ simple_vector.py
@@ -3,13 +3,34 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from langchain_openai import OpenAIEmbeddings  # Updated import
+from langchain.evaluation import load_evaluator
 from langchain_community.vectorstores import Chroma
 import openai 
 from dotenv import load_dotenv
 import os
 import shutil
 
+# loading environment variables from .env file
+load_dotenv()
+
+# setting OpenAI API key
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# initializing the embedding function
 embedding_function = OpenAIEmbeddings()
+
+# generating the embedding for the query
 vector = embedding_function.embed_query("apple")
 
-print(vector)
+# printing the length of the vector
+# print(vector)
+print(len(vector))
+
+# more interesting to find distance between vectors
+evaluator = load_evaluator("pairwise_embedding_distance")
+
+# to run an evaluation
+x = evaluator.evaluate_string_pairs(prediction="apple", prediction_b="orange")
+
+# print the evaluation result
+print(x)
